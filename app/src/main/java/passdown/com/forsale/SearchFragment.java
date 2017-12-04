6de1839a53bc8e4365e684fd2b9a5a57ece03085
Nate Reyes <njr9@students.uwf.edu>
2017-12-03 22:30:51 -0600

    building search

--- app/src/main/java/passdown/com/forsale/SearchFragment.java
+++ app/src/main/java/passdown/com/forsale/SearchFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,13 +33,54 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        /*listViewPosts = (ListView)view.findViewById(R.id.listViewPosts);
-        postList = new ArrayList<>();*/
+        listViewPosts = (ListView)view.findViewById(R.id.listViewPosts);
+        postList = new ArrayList<>();
+        final EditText searchText = view.findViewById(R.id.editText);
+        final String text = searchText.getText().toString();
+        Button mSearch = view.findViewById(R.id.Search);
+        mSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(isEmpty(text)){
+                    Toast.makeText(getActivity(), "searching", Toast.LENGTH_SHORT).show();
+                    search(text);
+                }else {
+                    Toast.makeText(getActivity(), "you must fill out search", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
         return view;
     }
 
-    public void search(){
+    public void search(final String search){
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot child : dataSnapshot.getChildren() ){
+                    Toast.makeText(getActivity(), "testing"+child.getKey(), Toast.LENGTH_SHORT).show();
+                    for (DataSnapshot childPost : child.getChildren()){
+                        Post post = childPost.getValue(Post.class);
+                        if(post.getTitle().equalsIgnoreCase(search)){
+                            postList.add(post);
+                        }
+                    }
 
+                }
+                PostList adapter = new PostList(getActivity(), postList);
+                listViewPosts.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {}
+        });
+
+    }
+
+    private boolean isEmpty(String string){
+        return string.equals("");
     }
 
     /*@Override
